58681b5af46517e0b80f368130a19b20ffa09b9c
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2022-05-31 00:44:33 -0500

    Add: Botones para seleccionar boletos

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,13 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text , StyleSheet} from 'react-native'
+import React , {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import CartItem from '../components/CartItem'
 
 
 
 export default function CartScreen() {
+  const [values, setValues] = useState(ticketsInputValues)
+
+  console.log(values);
   return (
-    <SafeAreaView>
+    <SafeAreaView styles={styles.safeContainer}>
       <Text>CartScreen</Text>
+      <CartItem  ticketName = 'vip1' changeValue = {(ticketCount)=>setValues(ticketCount)}  ticketObj= {values} />
+      <CartItem  ticketName = 'vip2' changeValue = {(ticketCount)=>setValues(ticketCount)}  ticketObj= {values} />
+      <Text>{values.vip1}</Text>
+      
     </SafeAreaView>
   )
 }
+
+const ticketsInputValues = {
+  vip1: 0,
+  vip2: 0,
+  vip3: 0
+}
+
+const styles = StyleSheet.create({
+
+  safeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
