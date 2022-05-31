08c12995b39223b68761ecb35a55d758123440e4
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2022-05-31 13:48:25 -0500

    Add: Boton atras CheckputScreen

--- src/screens/CheckOutScreen.js
+++ src/screens/CheckOutScreen.js
@@ -1,9 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
-export default function CheckOutScreen() {
+export default function CheckOutScreen({ route, navigation }) {
+    const { totalPrice } = route.params;
+
+    console.log(totalPrice);
   return (
     <View>
+      <Button title="Atras" onPress={() => navigation.goBack()} />
+
       <Text>CheckOutScreen</Text>
     </View>
   )
