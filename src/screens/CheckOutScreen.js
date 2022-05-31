83afccdfc4534b6576b251cf5983552498df98b5
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2022-05-31 13:36:46 -0500

    Add: Metodo de pago

--- src/screens/CheckOutScreen.js
+++ src/screens/CheckOutScreen.js
@@ -1,16 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, Button, Image, StyleSheet, TextInput } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function CheckOutScreen(props) {
-  const {route:{totalPrice}} = props
+export default function CheckOutScreen({ route, navigation }) {
+  const { totalPrice } = route.params;
 
   console.log(totalPrice);
   return (
-
-    
     <SafeAreaView>
-      <Text>CheckOutScreen</Text>
+      <Button title="Atras" onPress={() => navigation.goBack()} />
+      <View style={style.container}>
+        <Image
+          style={style.cardImage}
+          source={require("../../assets/creditCard.png")}
+        />
+        <Text>Numero Tarjeta</Text>
+        <TextInput placeholder="xxxx-xxxx-xxxx-xxxx" />
+        <Text>Titular</Text>
+        <TextInput placeholder="Nombre Titular" />
+        <Text>CCV</Text>
+        <TextInput placeholder="CCV" />
+        <Text>TOTAL: {totalPrice}</Text>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
+
+const style = StyleSheet.create({
+  container: {
+    display: "flex",
+    alignItems: "center",
+  },
+  cardImage: {
+    width: "80%",
+    height: 180,
+    resizeMode: "contain",
+  },
+});
