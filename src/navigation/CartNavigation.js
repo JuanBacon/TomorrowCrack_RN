65648ca86f7209a97267247acc0992c2166e86d9
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2022-05-31 13:42:24 -0500

    Add: Ruta checkout

--- src/navigation/CartNavigation.js
+++ src/navigation/CartNavigation.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import CartScreen from '../screens/CartScreen';
 import PaymentScreen from '../screens/PaymentScreen';
+import CheckOutScreen from '../screens/CheckOutScreen';
 
 
 const Stack = createStackNavigator();
@@ -12,6 +13,8 @@ export default function CartNavigation() {
     <Stack.Navigator  screenOptions={{headerShown: false}}>
         <Stack.Screen name='Carrito' component={CartScreen} />
         <Stack.Screen name='Payment' component={PaymentScreen} />
+        <Stack.Screen name='CheckOut' component={CheckOutScreen} />
+
 
     </Stack.Navigator>
     )
