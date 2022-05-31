83593008b4dc2d7865bee276f2a4a9b5c3d5892c
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2022-05-31 12:02:34 -0500

    add: scroll home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,75 +1,103 @@
-import { View, Text, StatusBar, Image, StyleSheet } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  View,
+  Text,
+  StatusBar,
+  Image,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-SafeAreaView
+SafeAreaView;
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.appBackground}>
       <StatusBar barStyle="light-content" backgroundColor={"#302B4F"} />
-      <Image source={require('../../assets/GifMuestra2.gif')} style={styles.video} resizeMode="cover"></Image>
+      <Image
+        source={require("../../assets/GifMuestra2.gif")}
+        style={styles.video}
+        resizeMode="cover"
+      ></Image>
       <Text style={styles.tittle}>TOMORROWCRACK'S</Text>
-      <View style={{display: "flex", alignItems: "center",}}>
-        <View style={styles.block}>
-          <Text style={styles.subtittle}>Descripción</Text>
-          <Text style={styles.text}>Este es el evento más importante del país, donde se reúnen los más cracks de la música y tomarse el gran escenario y hacer de tu noche una de las mejores. <Text style={{fontWeight: "bold"}}>¡Este año se celebra la edición 2022 del festival!</Text></Text>
+      <ScrollView style={{marginBottom:50}} >
+        <View style={{ display: "flex", alignItems: "center" }}>
+          <View style={styles.block}>
+            <Text style={styles.subtittle}>Descripción</Text>
+            <Text style={styles.text}>
+              Este es el evento más importante del país, donde se reúnen los más
+              cracks de la música y tomarse el gran escenario y hacer de tu
+              noche una de las mejores.{" "}
+              <Text style={{ fontWeight: "bold" }}>
+                ¡Este año se celebra la edición 2022 del festival!
+              </Text>
+            </Text>
+          </View>
+          <View style={styles.block}>
+            <Text style={styles.subtittle}>Fecha, Hora y Lugar</Text>
+            <Text style={styles.text}>
+              Esta edición del TomorrowCrack's Festival llegará a Cali el día{" "}
+              <Text style={{ fontWeight: "bold" }}>31 de Mayo del 2022</Text> a
+              las <Text style={{ fontWeight: "bold" }}>6:00PM</Text> en el
+              Autocine PiedraGrande
+            </Text>
+            <Image
+              source={require("../../assets/place.png")}
+              resizeMode="contain"
+              style={styles.placeImg}
+            ></Image>
+          </View>
         </View>
-        <View style={styles.block}>
-          <Text style={styles.subtittle}>Fecha, Hora y Lugar</Text>
-          <Text style={styles.text}>Esta edición del TomorrowCrack's Festival llegará a Cali el día <Text style={{fontWeight: "bold"}}>31 de Mayo del 2022</Text> a las <Text style={{fontWeight: "bold"}}>6:00PM</Text> en el Autocine PiedraGrande</Text>
-          <Image source={require('../../assets/place.png')} resizeMode="contain" style={styles.placeImg}></Image>
-        </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   appBackground: {
     backgroundColor: "#06021B",
-    height: "100%"
+    height: "100%",
   },
   video: {
     position: "absolute",
     width: "100%",
     height: "100%",
-    opacity: 0.1
+    opacity: 0.1,
   },
   tittle: {
     fontSize: 35,
-    textAlign: 'center',
+    textAlign: "center",
     color: "white",
     padding: 40,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   subtittle: {
     fontSize: 35,
-    textAlign: 'left',
+    textAlign: "left",
     color: "white",
     padding: 20,
-    fontWeight: "medium"
-  }, 
-  text:{
+    fontWeight: "medium",
+  },
+  text: {
     fontSize: 20,
-    textAlign: 'left',
+    textAlign: "left",
     color: "white",
     fontWeight: "200",
-    paddingBottom: 20
+    paddingBottom: 20,
   },
   block: {
     backgroundColor: "rgba(48,43,79,0.7)",
     borderRadius: 50,
     width: "90%",
     display: "flex",
-    alignItems:"center",
+    alignItems: "center",
     paddingLeft: 44,
     paddingRight: 44,
-    marginVertical: 10
-    
+    marginVertical: 10,
   },
   placeImg: {
     height: "40%",
-    width: "100%"
-  }
+    width: "100%",
+  },
 });
